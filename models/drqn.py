43cb168aff44895c7ecfb778beeba5185b160057
jeremy <ajcantor1@gmail.com>
2022-12-07 14:27:10 -0500

    add drqn implementation

--- models/drqn.py
+++ models/drqn.py
@@ -65,4 +65,4 @@ class DRQN(BaseDQN):
     if training is True:
         return torch.zeros([1, batch_size, self.hidden_space]), torch.zeros([1, batch_size, self.hidden_space])
     else:
-        return torch.zeros([1, 1, self.hidden_space]), torch.zeros([1, 1, self.hidden_space])
+        return torch.zeros([1, self.hidden_space]), torch.zeros([1, self.hidden_space])
