2d60c88844948acbeede7120d2503cf5b368f0fc
jeremy <ajcantor1@gmail.com>
2022-12-07 12:55:02 -0500

    add drqn implementation

--- models/basedqn.py
+++ models/basedqn.py
@@ -11,18 +11,34 @@ class BaseDQN(nn.Module):
   @property
   def height(self):
     return self._height
+
+  @height.setter
+  def height(self, _height):
+    self._height = _height
   
   @property
   def width(self):
     return self._width
 
+  @width.setter
+  def width(self, _width):
+    self._width = _width
+
   @property
   def channels(self):
     return self._channels
 
+  @channels.setter
+  def channels(self, _channels):
+    self._channels = _channels
+
   @property
-  def _outputs(self):
+  def outputs(self):
     return self._outputs
+
+  @outputs.setter
+  def outputs(self, _outputs):
+    self._outputs = _outputs
     
   def __init__(self, _channels, _height, _width, _outputs):
     super().__init__()
