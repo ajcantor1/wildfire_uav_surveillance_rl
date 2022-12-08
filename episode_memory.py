ea8af025eba20f2e8aed05748150d3d5d0cd5b12
jeremy <ajcantor1@gmail.com>
2022-12-07 22:13:34 -0500

    add drqn implementation

--- episode_memory.py
+++ episode_memory.py
@@ -11,7 +11,7 @@ class EpisodeMemory:
     self._episode = ReplayMemory(capicity)
 
   def push(self, *args):
-    self._episode.push(Transition(*args))
+    self._episode.push(Transition(*list(args)))
 
   def __getitem__(self, index):
     return self._episode[int(index)]
