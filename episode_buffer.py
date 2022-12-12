6699d3597ecb77532151f870465389da36250b80
jeremy <ajcantor1@gmail.com>
2022-12-12 14:55:09 -0500

    Added test notebook for  DRQN

--- episode_buffer.py
+++ episode_buffer.py
@@ -4,21 +4,26 @@ import random
 class EpisodeBuffer:
 
 
-  def __init__(self, capacity=20):
+  def __init__(self, capacity=50):
     self.capicity = capacity
     self.episodes = deque([],maxlen=self.capicity)
 
   def push(self, episode):
     self.episodes.append(episode)
 
-  def sample(self, batch_size, episode_start, episode_end):
+  def sample(self, batch_size, episode_length):
     
     episode_batch = random.sample(self.episodes, batch_size)
+
+    for i in range(batch_size):
+        episode_start = random.randint(0, 60)
+        episode_end  =  min(episode_start+episode_length, len(episode_batch[i]))
+        episode_batch[i] = episode_batch[i][episode_start:episode_end]
     
-    min_episode_length = min([len(episode) for episode in episode_batch] + [episode_end-episode_start])
+    min_episode_length = min([len(episode) for episode in episode_batch])
     
     for i in range(batch_size):
-      episode_batch[i] = episode_batch[i][episode_start:episode_start+min_episode_length]
+      episode_batch[i] = episode_batch[i][0:min_episode_length]
 
     return episode_batch, min_episode_length
 
