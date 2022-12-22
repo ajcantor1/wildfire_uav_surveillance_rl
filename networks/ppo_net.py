25c158ec8ff9d005363b875a5bf8d1c7e422cf58
jeremy <ajcantor1@gmail.com>
2022-12-22 14:00:20 -0500

    Added independent PPO

--- networks/ppo_net.py
+++ networks/ppo_net.py
@@ -1,12 +1,15 @@
 import torch
 import torch.nn as nn
 from networks.basedqn import BaseDQN
-
+from torch.distributions import Categorical
+import torch.nn.functional as F
 class PPONet(BaseDQN):
 
-  def __init__(self, _device, _channels, _height, _width, _outputs, _hidden_dim = 200):
+  def __init__(self, _device, _channels, _height, _width, _outputs):
 
     super().__init__(_device, _channels, _height, _width, _outputs)
+
+    super().__init__(_channels, _height, _width, _outputs)
 
     self.fc1  = nn.Sequential(
       nn.Linear(5, 100),
@@ -41,25 +44,18 @@ class PPONet(BaseDQN):
       nn.ReLU(),
     )
 
-    self.ltsm = nn.LSTM(_hidden_dim, _hidden_dim, batch_first=True)
+    
 
-    self.fc3 = nn.Sequential(
-      nn.Linear(_hidden_dim, _outputs),
-      nn.ReLU()
-    )
+    self.actor = nn.Linear(200, _outputs)
 
+    self.critc = nn.Linear(200, 1)
 
-  def forward(self, belief_map, state_vector, hidden = None):
+  def forward(self, belief_map, state_vector):
 
     fc1_out = self.fc1(state_vector)
-    conv_out = torch.flatten(self.conv(belief_map), 1)
+    conv_out = torch.flatten(self.conv(belief_map),1)
     fc2_out = self.fc2(conv_out)
-    new_hidden = None
-    ltsm_out = None
-    if hidden is None:
-      ltsm_out, new_hidden = self.ltsm(torch.cat((fc1_out, fc2_out), dim=1))
-    else:
-      ltsm_out, new_hidden = self.ltsm(torch.cat((fc1_out, fc2_out), dim=1), hidden)
-    fc3_out = self.fc3(ltsm_out)
-    return fc3_out, new_hidden
+    flattened_concatenated = torch.cat((fc1_out, fc2_out), dim=1)
+    return F.softmax(self.actor(flattened_concatenated), dim=1), self.critic(flattened_concatenated)
+
 
