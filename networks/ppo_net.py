9e91781c4dd8f2ed09e4a60fe6ff1309e978730e
jeremy <ajcantor1@gmail.com>
2022-12-24 10:49:15 -0500

    Added independent PPO

--- networks/ppo_net.py
+++ networks/ppo_net.py
@@ -42,21 +42,33 @@ class PPONet(BaseDQN):
       nn.ReLU(),
     )
 
+
     self.fc3 = nn.Sequential(
       nn.Linear(200, 200),
       nn.ReLU(),
     )
+    
+    self.ltsm = nn.LSTM(200, 200, batch_first=True)
 
     self.actor = nn.Linear(200, _outputs)
 
     self.critic = nn.Linear(200, 1)
 
-  def forward(self, belief_map, state_vector):
+  def forward(self, belief_map, state_vector, hidden=None):
 
     fc1_out = self.fc1(state_vector)
     conv_out = torch.flatten(self.conv(belief_map),1)
     fc2_out = self.fc2(conv_out)
     fc3_out = self.fc3(torch.cat((fc1_out, fc2_out), dim=1))
-    return self.actor(fc3_out), self.critic(fc3_out)
+
+    ltsm_out = None
+    new_hidden = None
+
+    if hidden is None:
+      ltsm_out, new_hidden = self.ltsm(fc3_out)
+    else:
+      ltsm_out, new_hidden = self.ltsm(fc3_out, hidden)
+
+    return self.actor(ltsm_out), self.critic(ltsm_out), new_hidden
 
 
